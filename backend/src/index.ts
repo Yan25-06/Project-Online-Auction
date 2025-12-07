714bd1b25d27b77eaa5af957db160a4d7d5cf274
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2025-12-07 12:22:03 +0700

    chore: setup backend + typescript + model controllers

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { exampleRouter } from './routes/example.route.js';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use(exampleRouter) 
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
