116c6238b39e4a4eeb3a20d2205fbe97fa640ee1
Yan25-06 <huytoan709@gmail.com>
2026-01-06 16:16:11 +0700

    fix: delete setupRoute

--- backend/src/helper/setupRoute.ts
+++
@@ -1,6 +0,0 @@
-import type { Application } from "express";
-import { exampleRouter } from "../routes/example.route.js"; 
-
-// export const setupRoute = (app:Application) => {
-//     app.use(exampleRouter);
-// }
