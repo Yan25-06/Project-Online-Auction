814824e705485bfd64fd544a20d05c3c74f5e6fa
Yan25-06 <huytoan709@gmail.com>
2026-01-06 16:56:52 +0700

    fix: all cors methods

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,7 +34,11 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  exposedHeaders: ['Content-Range', 'X-Content-Range'],
+  maxAge: 86400
 }));
 
 app.use(express.json());
